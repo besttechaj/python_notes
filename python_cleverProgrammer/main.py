e4af12c8f563211722c60db786ce6296ebce8143
Ajay Mishra <asap2656@gmail.com>
2023-08-23 09:28:52 +0530

    added some new functionality

--- python_cleverProgrammer/main.py
+++ python_cleverProgrammer/main.py
@@ -1,20 +1,27 @@
 # tip calculator App
 # float division (//) : divide two numbers and return the result after rounding off to its nearest value (ie. remove the float values)
 
-meal_Amount=float(input('Enter the meal amount: $'))
-# converting percentage into decimal 
-tip_Percentage=float(input('Enter the tip amount (in %) '))/100
-# to add an extra line
-print('\n')
-print('\n')
-print('\n')
-print('-----------------------------------')
-print(f'Food Amount: ${meal_Amount}')
-tip_Amount= meal_Amount*tip_Percentage
-print(f'Tip Amount: ${tip_Amount}')
-total_Amount=meal_Amount+tip_Amount
-# to add an extra line
-print('\n')
-print(f'Total pay: ${str(total_Amount)}')
-print('-----------------------------------')
+# meal_Amount=float(input('Enter the meal amount: $'))
+# # converting percentage into decimal 
+# tip_Percentage=float(input('Enter the tip amount (in %) '))/100
 
+# # datatype of argument should be float,int and return value must be an integer
+# def calculateFoodTotalAmount(meal_Amount:int,tip_Percentage:int) -> int:
+#     # # to add an extra line
+#     print('\n')
+#     print('\n')
+#     print('\n')
+#     print('-----------------------------------')
+#     print(f'Food Amount: ${meal_Amount}')
+#     tip_Amount= meal_Amount*tip_Percentage
+#     print(f'Tip Amount: ${tip_Amount}')
+#     total_Amount=meal_Amount+tip_Amount
+#     # to add an extra line
+#     print('\n')
+#     print(f'Total pay: ${str(total_Amount)}')
+#     print('-----------------------------------')
+
+# calculateFoodTotalAmount(meal_Amount,tip_Percentage)
+# ============================================================
+
+1hr 51min
